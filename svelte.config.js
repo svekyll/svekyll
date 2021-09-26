5ff68f9f7318b705319a36f4f6b4cf4478d38098
Chris Dawson <xrdawson@gmail.com>
2021-09-26 09:04:14 -0700

    Add js-yaml and _config.yml processing

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,15 @@
 import preprocess from "svelte-preprocess";
+import fs from 'fs';
+import yaml from 'js-yaml';
+
+try {
+    const doc = yaml.load(fs.readFileSync('_config.yml', 'utf8'));
+    process.env.VITE_JEKYLL_CONFIG = JSON.stringify(doc);
+} catch (err) {
+    console.log('Unable to process _config.yml, skipping (you may not have a _config.yml and that is fine)');
+}
+// console.log("ENV: ", JSON.stringify(process.env));
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
