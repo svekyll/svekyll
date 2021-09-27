e25815b2af47e1d142dab85400faf7d1cc37e675
Chris Dawson <xrdawson@gmail.com>
2021-09-27 15:55:51 -0700

    Fix analytics

--- src/lib/Analytics.svelte
+++ src/lib/Analytics.svelte
@@ -2,6 +2,7 @@
 	import { config } from './config';
 	import { browser } from '$app/env';
 	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
 
 	function fixHost(host) {
 		if (!host.startsWith('//') && !host.startsWith('http')) {
@@ -11,6 +12,26 @@
 		}
 	}
 
+	onMount(() => {
+		if (config.analytics && config.analytics.matomo) {
+			var _paq = (window._paq = window._paq || []);
+			_paq.push(['trackPageView']);
+			_paq.push(['enableLinkTracking']);
+			(function () {
+				var u = fixHost(config.analytics.matomo.url);
+				_paq.push(['setTrackerUrl', u + 'matomo.php']);
+				_paq.push(['setSiteId', config.analyics.matomo.site_id]);
+				var d = document,
+					g = d.createElement('script'),
+					s = d.getElementsByTagName('script')[0];
+				g.async = true;
+				g.src = u + 'matomo.js';
+				conf;
+				s.parentNode.insertBefore(g, s);
+			})();
+		}
+	});
+
 	function trackPageChange() {
 		if (window._paq) {
 			const { pathname } = window.location;
@@ -19,30 +40,5 @@
 		}
 	}
 
-    $: $page.path, browser && trackPageChange();
-
+	$: $page.path, browser && trackPageChange();
 </script>
-
-<svelte:head>
-	{#if config.analytics}
-		{#if config.provider === 'matomo'}
-			<script>
-				var _paq = (window._paq = window._paq || []);
-				_paq.push(['trackPageView']);
-				_paq.push(['enableLinkTracking']);
-				(function () {
-					var u = {`${fixHost(config.analytics.matomo.url)}`};
-					_paq.push(['setTrackerUrl', u + 'matomo.php']);
-					_paq.push(['setSiteId', [`${config.analyics.matomo.site_id}`}]);
-					var d = document,
-						g = d.createElement('script'),
-						s = d.getElementsByTagName('script')[0];
-					g.async = true;
-					g.src = u + 'matomo.js';
-					conf;
-					s.parentNode.insertBefore(g, s);
-				})();
-			</script>
-		{/if}
-	{/if}
-</svelte:head>
